1733de183a8781c6a903a3becba9dbb678ace3ca
Jaqueline <jaqui.sm5@icloud.com>
2022-09-29 23:14:46 -0500

    homework unit tests

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -13,6 +13,30 @@ class TestZookeeper(unittest.TestCase):
             tree = Ztree()
             tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
 
+    def test_show_data(self):
+        tree = Ztree()
+        tree.create('/node4', 'test', True, True, 20, '/')
+        self.assertEqual(tree.showNode('/node4'), None)  
+
+    def test_doesnt_exist(self):
+        tree = Ztree()
+        tree.create('/node2', 'test', True, False, 20, '/')
+        self.assertNotEqual(tree.getData('/node3'), 'test')     
+
+    def test_create_node_false(self):
+        tree = Ztree()
+        tree.create('/node2', 'test', False, False, 20, '/')
+        self.assertEqual(tree.getData('/node2'), 'test') 
+    
+    def test_delete_node(self):
+        tree = Ztree()
+        tree.create('/node1/node2/node3', 'test', True, False, 20, '/')
+        self.assertFalse(tree.delete('/node3', 0))
+    
+    def test_children(self):
+        tree = Ztree()
+        tree.create('/node3', 'test', True, False, 20, '/')
+        self.assertEqual(tree.getChildren('/node3'), None)
 
 if __name__ == '__main__':
     unittest.main()
